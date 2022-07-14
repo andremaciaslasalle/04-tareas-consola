c6e63d91099d49c03360346f7ff4fa1c5cde552c
André Macías <andremacias@lasallistas.org.mx>
2022-07-13 23:23:26 -0500

    Es el penúltimo commit

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 require("colors");
 
+const { guardarDB,leerDB } = require("./helpers/guardarArchivo");
 //const {mostrarMenu,pausa } = require("./helpers/mensajes");
-const {inquirerMenu,pausa,leerInput } = require("./helpers/inquirer");
+const {inquirerMenu,pausa,leerInput,listadoTareasBorrar,confirmar,mostrarListadoCheckList } = require("./helpers/inquirer");
 const Tarea = require("./models/tarea");
 const Tareas = require("./models/tareas");
 
 const main = async() =>{
     let menu = "";
     const tareas = new Tareas();
+    const tareasDB = leerDB();
+    if(tareasDB){
+        tareas.cargarTareasFromArray(tareasDB);
+    }
     do{
         menu = await inquirerMenu();
         switch(menu){
-            case "1":
+            case "1": //crear tarea
                 const descripcion = await leerInput("Descripción:");
                 tareas.crearTarea(descripcion);
                 break;
-            case "2":
-                console.log(tareas._listado);
+            case "2": //listar tareas
+                tareas.listadoCompleto();
+                break;
+            case "3": //listar tareas completadas
+                tareas.listarPendientesCompletadas(true);
+                break;
+            case "4": //listar tareas pendientes
+                tareas.listarPendientesCompletadas(false);
+                break;
+            case "5": //completar tareas
+                const ids = await mostrarListadoCheckList(tareas.listadoArr);
+                console.log(ids);
+                break;
+            case "6": //borrar tareas
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if(id !== "0"){
+                    const ok = await confirmar(`¿Está seguro?`);
+                    if(ok){
+                        tareas.borrarTarea(id);
+                        console.log("Tarea borrada");
+                    }
+                }
                 break;
         }
+        guardarDB(tareas.listadoArr);
         await pausa();
     }while(menu!=="0");
 }
--- models/tareas.js
+++ models/tareas.js
@@ -3,13 +3,74 @@ const Tarea = require("./tarea");
 class Tareas{
     _listado = {};
 
+    get listadoArr(){
+        const listado = [];
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        })
+        return listado;
+    }
+
     constructor(){
         this._listado = {};
+    }
+
+    borrarTarea( id = ""){
+        if(this._listado[id]){
+            delete this._listado[id];
+        }
     }
 
     crearTarea(desc){
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
+    }
+
+    cargarTareasFromArray(tareas = []){
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        });
+    }
+
+    listadoCompleto(){
+       /*  const lista = this.listadoArr;
+        let contador = "";
+        let estatus = "";
+        for(let i=0; i<lista.length;i++){
+            if(lista[i].completadoEn != null){
+                contador = `${i+1}`.green;
+                estatus = "Completada".green;
+            }else{
+                contador = `${i+1}`;
+                estatus = "Pendiente".red;
+            }
+            console.log(`${contador}. ${lista[i].desc} :: ${estatus}`);
+        } */
+        console.log("");
+        this.listadoArr.forEach((tarea,idx) =>{
+            const i = `${idx+1}.`.green;
+            const {desc,completadoEn } = tarea;
+            const estado = (completadoEn)?"Completada".green:"Pendiente".red;
+            console.log(`${i} ${desc} :: ${estado}`);
+        });
+    }
+
+    listarPendientesCompletadas(completadas=true){
+        console.log("");
+        let contador = 0;
+        this.listadoArr.forEach(tarea => {
+            const {desc,completadoEn} = tarea;
+            if(completadas && completadoEn){
+                contador += 1;
+                const i = `${contador}.`.green;
+                console.log(`${i} ${desc} :: ${completadoEn.green}`);
+            }else if(!completadas && !completadoEn){
+                contador += 1;
+                const i = `${contador}.`.green;
+                console.log(`${i} ${desc} :: ${"Pendiente".red}`);
+            }
+        })
     }
 }
 
